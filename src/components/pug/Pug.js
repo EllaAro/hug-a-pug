9021f62f3c1a0d53675c4ea66f83940e924811e8
Ella Aronov <ellaarono1994@gmail.com>
2020-04-27 18:20:52 +0300

    Updated the Pug component, changed the pugData file to fit the requirments

--- src/components/pug/Pug.js
+++ src/components/pug/Pug.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import PropTypes from 'prop-types'
 import './Pug.css'
 
 function Pug ({pugInfo}) {
@@ -6,13 +7,24 @@ function Pug ({pugInfo}) {
 
     return (
         <div 
-            className="image-container"
+            className='image-container'
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
         >
-            <img src={pugInfo.url} className="image-grid" alt={pugInfo.name}/>
+            {hovered && <i className='far fa-grin-hearts favorite'></i>}
+            {hovered && <i class='fas fa-dog choose'></i>}
+            <img src={pugInfo.url} className='image-grid' alt={pugInfo.name}/>
         </div>
     )
+}
+
+Pug.propTypes = {
+    pugInfo: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string,
+        url: PropTypes.string.isRequired,
+        expenses: PropTypes.number.isRequired,
+      }),
 }
 
 export default Pug
