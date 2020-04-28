bd61f5edf11312a7ec51206cfe4cd9db6d2aa2c9
Ella Aronov <ellaarono1994@gmail.com>
2020-04-28 16:24:58 +0300

    Added The 'Submit A Request' Button on the pugChosen page

--- src/context/ContextPugChosen.js
+++ src/context/ContextPugChosen.js
@@ -9,17 +9,20 @@ function ContextPugChosenProvider ({children}) {
     const updateChosenPugs = (newChosenPug) => setChosenPugs( prevChosenPugs => setChosenPugs([...prevChosenPugs,newChosenPug]));
      
     const removeChosenPug = (removedChosenPug) => {
-        setChosenPugs(prevChosenPugs => prevChosenPugs.filter(pug => pug.id !== removedChosenPug.id))
+        setChosenPugs(prevChosenPugs => prevChosenPugs.filter(pug => pug.id !== removedChosenPug.id));
      }
     
     const sumOfExpenses = () => {
         if (chosenPugs.length > 0) return chosenPugs.map(pug => pug.expenses).reduce((prev, next) => prev + next).toFixed(2);
-        return 0;
-        
+        return 0 ;   
+    }
+
+    const isChosenPugsEmpty = () => {
+        return (chosenPugs === undefined || chosenPugs.length === 0);
     }
 
     return (
-        <Context.Provider value={{chosenPugs, updateChosenPugs, removeChosenPug, sumOfExpenses}}>
+        <Context.Provider value={{chosenPugs, updateChosenPugs, removeChosenPug, sumOfExpenses, isChosenPugsEmpty}}>
             {children}
         </Context.Provider>
     )
--- src/pages/PugChosen.js
+++ src/pages/PugChosen.js
@@ -4,7 +4,7 @@ import ChosenPug from '../components/chosenPug/ChosenPug'
 import  '../App.css'
 
 function PugChosen () {
-    const {chosenPugs, sumOfExpenses} = useContext(ContextPugChosen);
+    const {chosenPugs, sumOfExpenses, isChosenPugsEmpty} = useContext(ContextPugChosen);
     const chosen = chosenPugs.map(pug => <ChosenPug key={pug.id} chosenPug={pug}/>);
 
     return (
@@ -12,6 +12,11 @@ function PugChosen () {
             <h1>Chosen Pugs</h1>
             {chosen}
             <h3>Total Cost Of Expenses: {sumOfExpenses()}</h3>
+            <div className="request-btn">
+                {!isChosenPugsEmpty() &&
+                <button className='myButton'>Submit A Request</button>}
+                
+            </div>
         </main>
     )
 }
